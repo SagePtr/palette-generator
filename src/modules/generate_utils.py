94006c5fd0083a1f2a1ae7aad8bf9a6bb315dbdc
SagePtr <sageptr@gmail.com>
2018-01-10 19:00:46 +0900

    Remove module no longer used in project

--- src/modules/generate_utils.py
+++
@@ -1,8 +0,0 @@
-# Utility functions for geneators
-
-def colorhex(color):
-    result = '%02X%02X%02X' % tuple(color[:3])
-    if len(result) != 6:
-        raise Exception('Invalid color range')
-    return result
-
